4ad5d37f2714955b5e88c1ca22e5333ce2b043f5
Nelson Neto <nelson.ash@outlook.com>
2024-07-09 17:20:42 -0300

    2-Criacao 2 casos de teste

--- Manager.Tests/UnitTests/AccountsControllerTests.cs
+++ Manager.Tests/UnitTests/AccountsControllerTests.cs
@@ -6,4 +6,14 @@ public class AccountsControllerTests
     public void Given_When_Then()
     {
     }
+
+    [Fact]
+    public void Dado_Requisicao_Get_When_Request_Valido_Then_Retorna_Ok()
+    {
+    }
+
+    [Fact]
+    public void Dado_Requisicao_Get_When_Request_Invalido_Then_Retorna_BadRequest()
+    {
+    }
 }
