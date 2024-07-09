5a45c174ac9b1a386d368d326fd32908687bd02b
Nelson Neto <nelson.ash@outlook.com>
2024-07-09 17:35:42 -0300

    Criacao do Tests de Result 1 positivo e 1 Negativo

--- Manager.Tests/UnitTests/AccountsControllerTests.cs
+++ Manager.Tests/UnitTests/AccountsControllerTests.cs
@@ -1,4 +1,6 @@
 using Manager.Api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace Manager.Tests.UnitTests;
 
@@ -19,10 +21,14 @@ public class AccountsControllerTests
     [Fact]
     public void Dado_Requisicao_Get_When_Request_Valido_Then_Retorna_Ok()
     {
+        var retorno = (StatusCodeResult)_sut.Get();
+        Assert.Equal((int)HttpStatusCode.OK, retorno.StatusCode);
     }
 
     [Fact]
     public void Dado_Requisicao_Get_When_Request_Invalido_Then_Retorna_BadRequest()
     {
+        var retorno = (StatusCodeResult)_sut.Get();
+        Assert.NotEqual((int)HttpStatusCode.BadRequest, retorno.StatusCode);
     }
 }
